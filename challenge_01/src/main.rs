89150b7118c30b3f93adf4e1639aa9cecd5a7030
Loïc Riegel <loic.riegel@outlook.fr>
2024-12-01 13:05:31 +0100

    cargo fmt

--- challenge_01/src/main.rs
+++ challenge_01/src/main.rs
@@ -2,26 +2,22 @@ use std::fs::read_to_string;
 
 const INPUT_FILE_NAME: &str = "input.txt";
 
-
 #[derive(Debug, Default)]
 struct Input {
     first_list: Vec<u32>,
     second_list: Vec<u32>,
 }
 
-
 fn read_input(filename: &str) -> Input {
     let mut input = Input::default();
 
-    read_to_string(filename).unwrap().lines().for_each(
-        |line| {
-            let mut line_split = line.split("   ");
-            let first_nb: u32 = line_split.next().unwrap().parse().unwrap();
-            let second_nb: u32 = line_split.next().unwrap().parse().unwrap();
-            input.first_list.push(first_nb);
-            input.second_list.push(second_nb);
-        }
-    );
+    read_to_string(filename).unwrap().lines().for_each(|line| {
+        let mut line_split = line.split("   ");
+        let first_nb: u32 = line_split.next().unwrap().parse().unwrap();
+        let second_nb: u32 = line_split.next().unwrap().parse().unwrap();
+        input.first_list.push(first_nb);
+        input.second_list.push(second_nb);
+    });
 
     input
 }
