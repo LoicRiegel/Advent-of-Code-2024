45817b6a70fec30d10b8e769348225a481bd3da8
Loïc Riegel <loic.riegel@outlook.fr>
2024-12-01 14:28:21 +0100

    working

--- challenge_01/src/main.rs
+++ challenge_01/src/main.rs
@@ -1,28 +1,39 @@
-use std::fs::read_to_string;
+use std::{fs::read_to_string, iter::zip};
 
 const INPUT_FILE_NAME: &str = "input.txt";
 
-#[derive(Debug, Default)]
-struct Input {
-    first_list: Vec<u32>,
-    second_list: Vec<u32>,
-}
-
-fn read_input(filename: &str) -> Input {
-    let mut input = Input::default();
+fn read_input(filename: &str) -> Vec<(u32, u32)> {
+    let mut first_list: Vec<u32> = Vec::new();
+    let mut second_list: Vec<u32> = Vec::new();
 
     read_to_string(filename).unwrap().lines().for_each(|line| {
         let mut line_split = line.split("   ");
         let first_nb: u32 = line_split.next().unwrap().parse().unwrap();
         let second_nb: u32 = line_split.next().unwrap().parse().unwrap();
-        input.first_list.push(first_nb);
-        input.second_list.push(second_nb);
+        first_list.push(first_nb);
+        second_list.push(second_nb);
     });
 
-    input
+    first_list.sort();
+    second_list.sort();
+
+    let mut two_lists: Vec<(u32, u32)> = Vec::new();
+    zip(first_list, second_list).for_each(|entry| two_lists.push(entry));
+    two_lists
+}
+
+fn total_distance(two_lists: Vec<(u32, u32)>) -> u64 {
+    let mut total_distance: u64 = 0;
+    two_lists.into_iter().for_each(|entry| {
+        let diff = entry.0.abs_diff(entry.1);
+        total_distance = total_distance + (diff as u64);
+    });
+    total_distance
 }
 
 fn main() {
-    let input = read_input(INPUT_FILE_NAME);
-    dbg!(&input);
+    let two_lists = read_input(INPUT_FILE_NAME);
+    dbg!(&two_lists);
+    let total_distance = total_distance(two_lists);
+    dbg!(total_distance);
 }
