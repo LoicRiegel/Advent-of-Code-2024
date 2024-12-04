c36de395b486556efec6c8afc09db0ad503cc571
Loïc Riegel <loic.riegel@outlook.fr>
2024-12-04 23:36:34 +0100

    challenge 3 part 1 OK

--- challenge_03/src/main.rs
+++ challenge_03/src/main.rs
@@ -1,3 +1,4 @@
+use regex::Regex;
 use std::fs::read_to_string;
 
 const INPUT_FILE_NAME: &str = "input.txt";
@@ -7,6 +8,15 @@ fn read_input(filename: &str) -> String {
 }
 
 fn main() {
-    let text =  read_input(INPUT_FILE_NAME);
-    dbg!(text);
+    let text = read_input(INPUT_FILE_NAME);
+
+    let re = Regex::new(r"mul\((\d+),(\d+)\)").expect("Invalid regex!");
+
+    let numbers: Vec<(u32, u32)> = re.captures_iter(&text).fold(vec![], |mut acc, cap| {
+        acc.push((cap[1].parse().unwrap(), cap[2].parse().unwrap()));
+        acc
+    });
+
+    let total: u32 = numbers.iter().fold(0, |acc, e| acc + (e.0 * e.1));
+    dbg!(&total);
 }
